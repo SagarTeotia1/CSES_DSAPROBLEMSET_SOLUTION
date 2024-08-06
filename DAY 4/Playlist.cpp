4764d6bceda8db472e3f01a22aa1fb9cd3a3b799
SagarTeotia1 <147220406+SagarTeotia1@users.noreply.github.com>
2024-08-06 10:49:37 +0530

    commit

--- DAY 4/Playlist.cpp
+++ DAY 4/Playlist.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
@@ -12,24 +12,27 @@ int main() {
     cin >> n;
     vector<long long> songs(n);
     
-    // Read song IDs
+    // Read the song IDs
     for (int i = 0; i < n; ++i) {
         cin >> songs[i];
     }
 
-    unordered_set<long long> unique_songs;
+    unordered_map<long long, int> last_seen;
     int start = 0;
     int max_length = 0;
 
     for (int end = 0; end < n; ++end) {
-        // Remove elements from the start of the window until the current song is unique
-        while (unique_songs.find(songs[end]) != unique_songs.end()) {
-            unique_songs.erase(songs[start]);
-            ++start;
+        long long current_song = songs[end];
+        
+        // If the song was seen before and is in the current window, move start
+        if (last_seen.find(current_song) != last_seen.end() && last_seen[current_song] >= start) {
+            start = last_seen[current_song] + 1;
         }
-        // Insert the current song into the set
-        unique_songs.insert(songs[end]);
-        // Update the maximum length
+
+        // Update the last seen index of the current song
+        last_seen[current_song] = end;
+        
+        // Calculate the length of the current window and update max_length
         max_length = max(max_length, end - start + 1);
     }
 
